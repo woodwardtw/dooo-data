b760594b1bb7ada1fb28d7a2c95d7ed3cf643b5e
woodwardtw <twwoodward@me.com>
2022-08-18 10:46:31 -0400

    functional
    http://multisitetwo.local:10013/wp-content/plugins/dooo-data/data/last-logins.php - seems secure

---
+++ data/last-logins.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * DoOO Data destination
+ *
+ * 
+ **/
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$bar = 'LAST LOGIN DATE, USERNAME, EMAIL, PRIMARY DOMAIN, DISK USAGE, START DATE/n
+2022-08-16,a,a@middlebury.edu,a.middcreate.net,7252M,2016-06-28
+2022-08-16,b,b@middlebury.edu,b.middcreate.net,1076M,2022-02-18
+2022-08-12,c,c@middlebury.edu,c.middcreate.net,791M,2022-06-21
+2022-08-11,d,d@middlebury.edu,d.middcreate.net,19904M,2019-02-07
+2022-08-09,e,e@gmail.com,e.middcreate.net,1692M,2017-08-10
+2022-08-07,f,f@middlebury.edu,f.middcreate.net,4307M,2020-06-25
+2022-08-07,g,g@middlebury.edu,g.middcreate.net,2318M,2021-11-15
+2022-08-02,h,h@middlebury.edu,h.middcreate.net,621M,2019-09-17
+2022-08-02,i,i@middlebury.edu,i.middcreate.net,2243M,2016-09-10';
--- index.php
+++ index.php
@@ -14,16 +14,18 @@ Text Domain: my-toolset
 */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-
 add_action('wp_enqueue_scripts', 'dooo_data_load_scripts');
 
 function dooo_data_load_scripts() {                           
     $deps = array('jquery');
     $version= '1.0'; 
     $in_footer = true;    
-    wp_enqueue_script('dooo_data-main-js', plugin_dir_url( _FILE_) . 'js/dooo-data-main.js', $deps, $version, $in_footer); 
-    wp_enqueue_style( 'dooo_data-main-css', plugin_dir_url( _FILE_) . 'css/dooo-data-main.css');
+    wp_enqueue_script('dooo_data-main-js', plugin_dir_url( __FILE__) . 'js/dooo-data-main.js', $deps, $version, $in_footer); 
+    wp_enqueue_style( 'dooo_data-main-css', plugin_dir_url( __FILE__) . 'css/dooo-data-main.css');
 }
+
+
+
 
 add_action('wp_dashboard_setup', 'dooo_data_dashboard_widgets');
   
@@ -34,18 +36,44 @@ function dooo_data_dashboard_widgets() {
       global $wp_meta_boxes;
       $domain = $_SERVER['SERVER_NAME'];
       $name = explode(".", $domain)[0];
-      wp_add_dashboard_widget('custom_dooo_widget', '<h2>DoOO Data</h2>', 'dooo_data_foo', '', '', 'column3', 'high');
-      var_dump($name . '_last_login.csv');
-      $root =  $_SERVER['DOCUMENT_ROOT'];
-      var_dump($root . 'root/' . $name . '_last_login.csv');
-      //Python stuff to  file in /root .... file_name = os.popen("echo $HOSTNAME").read().split(".")[0] + "_last_logins.csv"
-
+      wp_add_dashboard_widget('custom_dooo_widget', '<h2>DoOO Data</h2>', 'dooo_data_foo', '', '', 'column3', 'high');           
    }
   }
 //wp_add_dashboard_widget(  $widget_id,  $widget_name,  $callback,  $control_callback = null,  $callback_args = null,  $context = 'normal', string $priority = 'core' )
 
 function dooo_data_foo(){
-   echo 'foo';
+   require_once( plugin_dir_path( __FILE__ ) . 'data/last-logins.php' );
+   //$data = str_getcsv($bar);
+   $data = str_getcsv($bar, "\n");
+   $html = '';
+   //var_dump($data);
+   foreach ($data as $key=>$line) {
+       $row = explode(",", $line);
+       $date = $row[0];
+       $user = $row[1];
+       $email = $row[2];
+       $domain = $row[3];
+       $usage = $row[4];
+       $start = $row[5];
+       if($key === 0){
+         $html .="<table><tr>
+                  <th scope='col'>{$date}</th>
+                  <th scope='col'>{$user}</th>
+                  <th scope='col'>{$email}</th>
+                  <th scope='col'>{$domain}</th>
+                  <th scope='col'>{$usage}</th>
+               </tr>";
+       } else {
+         $html .="<tr>
+                  <td>{$date}</td>
+                  <td>{$user}</td>
+                  <td>{$email}</td>
+                  <td>{$domain}</td>
+                  <td>{$usage}</td>
+               </tr>";
+       }
+   }
+   echo $html . '</table>';
 }
 
 //LOGGER -- like frogger but more useful
